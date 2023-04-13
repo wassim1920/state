ef023eb342b6b3b90e07229eaa2b356005ce35b2
wassim hssin <wassim.hassiin@gmail.com>
2023-04-13 23:10:54 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'John Doe',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imgSrc: 'https://via.placeholder.com/150',
+      profession: 'Web Developer',
+    },
+    shows: false,
+    mountedAt: new Date(),
+  };
+
+  toggleShow = () => {
+    this.setState({ shows: !this.state.shows });
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const mountedAt = new Date(this.state.mountedAt);
+      mountedAt.setSeconds(mountedAt.getSeconds() + 1);
+      this.setState({ mountedAt });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        {this.state.shows && (
+          <div>
+            <h1>{fullName}</h1>
+            <img src={imgSrc} alt="profile" />
+            <p>{bio}</p>
+            <p>{profession}</p>
+          </div>
+        )}
+        <p>Component mounted at: {this.state.mountedAt.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
